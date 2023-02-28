33238aa745726492bae98d3638e0c92c0489b157
Nick Vazquez <nmv@iastate.edu>
2023-02-28 09:36:00 -0600

    Add Lab3

--- Lab3_All_Files/lab3/test/lab3/ProfileTest.java
+++ Lab3_All_Files/lab3/test/lab3/ProfileTest.java
@@ -6,14 +6,49 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ProfileTest {
-	  
-	   @Test
-	   public void test() {
-		   Profile profile = new Profile("Bull Hockey, Inc.");
-		   Question question = new BooleanQuestion(1, "Got bonuses?");
-		   Criteria criteria = new Criteria();
-		   Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-		   Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-		   criteria.add(criterion);
-		   }
-	   }
+	private Profile profile;
+	private BooleanQuestion question;
+	private Criteria criteria
+
+	@Before
+	public void create() {
+		profile = new Profile("Bull Hockey, Inc.");
+		question = new BooleanQuestion(1, "Got bonuses?");
+		criteria = new Criteria();
+	}
+
+	/**
+	 * Sets up a criteria that do not match this profiles' answer.
+	 *
+	 * @param questionWeight Weight of the mismatched question to add.
+	 *                       Determines how the mismatched test will fail.
+	 */
+	private void setUpMisMatchedProfileAndCriteria(Weight questionWeight) {
+		Answer profileAnswer = new Answer(question, Bool.FALSE);
+		profile.add(profileAnswer);
+
+		Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+		Criterion criterion = new Criterion(criteriaAnswer, questionWeight);
+		criteria.add(criterion);
+	}
+
+	@Test
+	public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
+		// Arrange
+		setUpMisMatchedProfileAndCriteria(Weight.MustMatch);
+		// Act
+		boolean matches = profile.matches(criteria);
+		// Assert
+		assertFalse(matches);
+	}
+
+	@Test
+	public void matchAnswersTrueForAnyDontCareCriteria() {
+		// Arrange
+		setUpMisMatchedProfileAndCriteria(Weight.DontCare);
+		// Act
+		boolean matches = profile.matches(criteria);
+		// Assert
+		assertTrue(matches);
+	}
+}
